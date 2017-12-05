19c93e639e8258df07f7937f2795eac78043cb24
jay26 <jayadi.kurniawan@gmail.com>
2017-12-05 10:30:03 +0700

    Add Flatlist view

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,12 @@
 import { StackNavigator } from 'react-navigation'
+import View from '../Containers/View'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  View: { screen: View },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
